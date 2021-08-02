290a8698d033230c8ff0fa71c30c3f4aeb2d8060
ffr <793943725@qq.com>
2021-08-02 12:04:58 +0800

    天气
    新一版首页

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,20 +1,70 @@
-// pages/main/main.js
+const APIKEY = "5f160e5759e04f97abd9cc9534d68af8";// 填入你申请的KEY
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    location:"117.20,36.20"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //this.getLocation()
+    this.getCityByLoaction();
+    this.getWeather();
   },
 
+  /**
+   * 根据坐标获取城市信息
+   */
+  getCityByLoaction() {
+    var that = this
+    wx.request({
+      url: 'https://geoapi.qweather.com/v2/city/lookup?key=' + APIKEY + "&location=" + that.data.location,
+      success(result) {
+        var res = result.data
+        if (res.code == "200") {
+          var data = res.location[0]
+          that.setData({
+            City: data.adm2,
+            County: data.name
+          })
+        } else {
+          wx.showToast({
+            title: '获取城市信息失败',
+            icon: 'none'
+          })
+        }
+
+      }
+    })
+  },
+  /**
+   * 获取天气
+   */
+  getWeather() {
+    var that = this
+    wx.request({
+      url: 'https://devapi.qweather.com/v7/weather/now?key=' + APIKEY + "&location=" + that.data.location,
+      success(result) {
+        var res = result.data
+        //console.log(res)
+        that.setData({
+          now: res.now
+        })
+      }
+    })
+  },
+  // 格式时间
+
+  // 补零
+  formatNumber(n) {
+    n = n.toString()
+    return n[1] ? n : '0' + n
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -64,7 +114,3 @@ Page({
 
   }
 })
-
-
-//天气预报
-
