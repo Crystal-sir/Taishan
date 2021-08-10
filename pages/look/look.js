5ca3b08a05c3ac46b23c8cc97ee3ac76ae5bd1d8
amazing-yin <421626388@qq.com>
2021-08-10 19:27:36 +0800

    v5.1

--- pages/look/look.js
+++ pages/look/look.js
@@ -1,76 +1,104 @@
-// pages/look/look.js
+const app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    popup: true
+	data: {
+		longitude: '',
+		latitude: '',
+		disKm:'',
+	},
+	onLoad() {
+		this.Location()
+		
+	},
+	regionchange(e) {
+		// 地图发生变化的时候，获取中间点，也就是用户选择的位置toFixed
+
+		if (e.type == 'end' && (e.causedBy == 'scale' || e.causedBy == 'drag')) {
+			var that = this;
+
+			this.mapCtx = wx.createMapContext("map4select");
+
+			this.mapCtx.getCenterLocation({
+
+				type: 'gcj02',
+
+				success: function(res) {
+
+					console.log(res, 11111)   //移动后，新位置的经纬度
+
+					that.setData({
+
+						latitude: res.latitude,
+
+						longitude: res.longitude
+
+						//circles: [{
+
+							//latitude: res.latitude,
+
+							//longitude: res.longitude,
+
+							// color: '#FF0000DD',
+
+							// fillColor: '#d1edff88',
+
+							// radius: 3000, //定位点半径
+
+							// strokeWidth: 1
+						//}]
+					})
+				}
+			})
+		}
+	},
+	//定位到自己的位置事件
+	my_location: function(e) {
+		var that = this;
+		that.onLoad();
+	},
+	//在onload中事件
+	Location: function() {
+		var that = this;
+		wx.getLocation({
+			type: "gcj02",
+			success: (res)=>  {
+				const latitude = res.latitude;
+	 	  	const longitude = res.longitude;
+				const resKm = that.getDistance(latitude,longitude,36.15,117.06);
+				that.setData({
+					latitude: res.latitude,
+					longitude: res.longitude,
+					disKm:resKm
+				})
+			}
+		})
+	},
+	rad (d) {//弧度转化
+    return d * Math.PI / 180.0;
+	},
+	//计算当前位置与山顶距离，其中山顶位置为玉皇极的位置.
+  getDistance(lat1, lng1, lat2, lng2) {
+    var radLat1 = this.rad(lat1);
+    var radLat2 = this.rad(lat2);
+    var a = radLat1 - radLat2;
+    var b = this.rad(lng1) - this.rad(lng2);
+    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
+    Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+    s = s * 6378.137; // EARTH_RADIUS;
+    s = Math.round(s * 10000) / 10000; //输出为公里
+
+    var distance = s;
+    var distance_str = "";
+
+    if (parseInt(distance) >= 1) {
+      distance_str = distance.toFixed(1) + "km";
+    } else {
+      distance_str = distance * 1000 + "m";
+    }
+    //s=s.toFixed(4);
+
+    console.info('距离是', s);
+    console.info('距离是', distance_str);
+    return s;
   },
-/* 隐藏弹窗 */
-hidePopup(flag = true) {
-  this.setData({
-      "popup": flag
-  });
-},
-/* 显示弹窗 */
-showPopup() {
-  this.hidePopup(false);
-},
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
